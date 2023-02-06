923559a37f4ec7fc9eca943a9523fbf85cb5669a
itsmeedward <86776978+itsmeedward@users.noreply.github.com>
2023-02-06 21:45:31 +0100

    Add files via upload

---
+++ Anasayfa.php
@@ -0,0 +1,332 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Best Way to your Own Homes</title>
+    <meta title="Best Way to your Own Homes">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Abel|Dosis:400,700|Indie+Flower|Oxygen:300,400,700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/9abaf1dfa2.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="Helvetica.ttf">
+    <link rel="stylesheet" href="../font-families/SF-Pro-Display-Light.otf">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="style2.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        body {
+            font-size: 16px;
+        }
+
+        @media screen and(max-width:700px) {
+            body {
+                font-size: 14px;
+                background: yellow;
+            }
+
+            img {
+                width: 60%;
+            }
+
+        }
+
+        @media screen and(max-width:400px) {
+            body {
+                font-size: 10px;
+            }
+
+            img {
+                width: 100%;
+            }
+
+        }
+
+    </style>
+</head>
+
+<body>
+
+
+    <!--<nav id="navbar navbar-expand-sm ">
+         
+               <div id="header-img">
+         <div id="fixed-nav">
+                <div class="list-container">
+                    <div class="logo-img">
+                        <img src="https://cdn.pixabay.com/photo/2016/10/02/19/51/lion-1710301_960_720.png" alt="" id="logo">
+
+                    </div>
+                    <ul class="">
+                       
+                           <li class="nav-item">
+                            <a href="" class="head-countainer-links nav-link">
+                                <i class="fas fa-home"></i>
+                                Anasayfa
+                            </a></li>
+                        <li class="list-items">
+                            <a href="" class="head-countainer-links nav-link">
+                                Hakkımzda
+                            </a></li>
+                        <li class="list-items"><a href="" class="nav-link head-countainer-links">
+                                İletişim
+                            </a></li>
+                        <li class="list-items" id="f-right"><a href="" class="nav-link head-countainer-links">
+                                Account
+                            </a></li>
+                    </ul>
+                 
+            </div>
+      
+        </div>
+    </nav>
+   
+
+
+    <nav class="navbar navbar-dark bg-dark navbar-expand-sm fixed-top justify-content-end">
+        <a href="" class="navbar-brand">Logo</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarcollapsebutton">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarcollapsebutton">
+            <ul class="navbar-nav">
+                <li class="nav-item "><a href="" class="nav-link text-white">Anasayfa</a></li>
+                <li class="nav-item"><a href="" class="nav-link text-white">Hakkımzda</a></li>
+                <li class="nav-item"><a href="" class="nav-link text-white">İletişim</a></li>
+                <li class="nav-item"><a href="" class="nav-link text-white">Hesap</a></li>
+            </ul>
+        </div>
+    </nav>-->
+<div class="w3-top">
+  <div class="w3-bar w3-black w3-card">
+    <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">HOME</a>
+    <a href="#band" class="w3-bar-item w3-button w3-padding-large w3-hide-small">BAND</a>
+    <a href="#tour" class="w3-bar-item w3-button w3-padding-large w3-hide-small">TOUR</a>
+    <a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-hide-small">CONTACT</a>
+    <div class="w3-dropdown-hover w3-hide-small">
+      <button class="w3-padding-large w3-button" title="More">MORE <i class="fa fa-caret-down"></i></button>     
+      <div class="w3-dropdown-content w3-bar-block w3-card-4">
+        <a href="#" class="w3-bar-item w3-button">Merchandise</a>
+        <a href="#" class="w3-bar-item w3-button">Extras</a>
+        <a href="#" class="w3-bar-item w3-button">Media</a>
+      </div>
+    </div>
+    <a href="javascript:void(0)" class="w3-padding-large w3-hover-red w3-hide-small w3-right"><i class="fa fa-search"></i></a>
+  </div>
+</div>
+    <div id="header-img"><img class="imgg" src="https://cdn.pixabay.com/photo/2016/06/24/10/47/architecture-1477041_960_720.jpg" alt="">
+        <div class="info">
+            <div class="container mt-4">
+                <div class="container bg-light text-center p-5" id="jv">
+                    <h2 class="text-center">İşte.İnanılmaz.Rüyanız.</h2>
+                    <!--<div class="zimage bg dark"><img style="width: 100%;" src="https://cdn.pixabay.com/photo/2013/07/12/17/49/estate-152518_960_720.png" alt=""></div>-->
+                    <p class="openingtext justify-content-center  pt-20">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt commodi, accusantium iure nihil quis ipsam ab nobis eaque! Ducimus suscipit saepe corporis soluta itaque, voluptas fugiat tenetur magni fugit nulla.</p>
+                    <button class="btn btn-light float-right p-1 " type="button">Daha Fazla Öğren <i class="fas fa-angle-double-right text-center"></i></button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+<div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="band">
+    <h2 class="w3-wide">THE al-Home&Real Estate</h2>
+    <p class="w3-opacity"><i>We love these Works</i></p>
+    <p class="w3-justify">We have created a fictional band website. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+      ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur
+      adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <div class="w3-row w3-padding-32">
+      <div class="w3-third">
+        <p>United Kingdom</p>
+        <img src="/w3images/bandmember.jpg" class="w3-round w3-margin-bottom" alt="This image is not found" style="width:60%">
+      </div>
+      <div class="w3-third">
+        <p>United States</p>
+        <img src="/w3images/bandmember.jpg" class="w3-round w3-margin-bottom" alt="This image is not found" style="width:60%">
+      </div>
+      <div class="w3-third">
+        <p>Canada</p>
+        <img src="/w3images/bandmember.jpg" class="w3-round" alt="This image is not found" style="width:60%">
+      </div>
+    </div>
+  </div>
+
+    <div class="card-container">
+        <div id="card1">
+            <div id="card-img"><img src="https://cdn.pixabay.com/photo/2013/10/09/02/27/boat-house-192990_960_720.jpg" alt=""></div>
+            <div id="card-title">
+                <h2 class="card-header">
+                    Card Header
+                    <h4>Aranılan en Mükemmel anlar için </h4>
+                </h2>
+
+                <p class="title">rem ipsum dolor sit amet, consectetur adipisicing elit. Autem omnis, recusandae libero, at magni quaerat eveniet. Laboriosam voluptatum repellat ut fugit, rerum ea molestiae possimus, magni illo quas dolor inventore illum aliquid dolore expedita ex quod omnis ullam dignissimos ipsum aperiam incidunt assumenda vero placeat. Vitae reprehenderit qui omnis possimus dolorum sed suscipit sint asperiores mollitia assumenda corporis eveniet esse odit illum, officiis aliquam commodi dolore et veritatis ea blanditiis similique itaque dolores nulla. Ducimus hic quasi magni, adipisci, numquam modi alias soluta consectetur consequuntur enim nihil nulla quam in. Optio velit iste, magnam laudantium quo sequi quibusdam officiis doloribus facere quia beatae id dolores nisi. Quaerat, pariatur commodi, corporis explicabo ipsum dolore inventore aliquid voluptate tempore! </p>
+                <button class="btn btn-success">Öğrenin</button>
+            </div>
+        </div>
+        <div id="card2">
+            <div id="card-img"><img src="https://cdn.pixabay.com/photo/2019/11/08/21/42/landscape-4612528_960_720.jpg" alt=""></div>
+            <div id="card-title">
+                <h2 class="card-header">
+                    Card Header
+                    <h4>Aranılan en Mükemmel anlar için </h4>
+                </h2>
+
+                <p class="title">rem ipsum dolor sit amet, consectetur adipisicing elit. Autem omnis, recusandae libero, at magni quaerat eveniet. Laboriosam voluptatum repellat ut fugit, rerum ea molestiae possimus, magni illo quas dolor inventore illum aliquid dolore expedita ex quod omnis ullam dignissimos ipsum aperiam incidunt assumenda vero placeat. Vitae reprehenderit qui omnis possimus dolorum sed suscipit sint asperiores mollitia assumenda corporis eveniet esse odit illum, officiis aliquam commodi dolore et veritatis ea blanditiis similique itaque dolores nulla. Ducimus hic quasi magni, adipisci, numquam modi alias soluta consectetur consequuntur enim nihil nulla quam in. Optio velit iste, magnam laudantium quo sequi quibusdam officiis doloribus facere quia beatae id dolores nisi. Quaerat, pariatur commodi, corporis explicabo ipsum dolore inventore aliquid voluptate tempore! </p>
+                <button class="btn btn-warning">Öğrenin</button>
+            </div>
+        </div>
+    </div>
+ <!-- The Tour Section -->
+  <div class="w3-black" id="tour">
+    <div class="w3-container w3-content w3-padding-64" style="max-width:800px">
+      <h2 class="w3-wide w3-center">Which Country Would You Like?</h2>
+      <p class="w3-opacity w3-center"><i>Remember to book your orders!</i></p><br>
+
+      <ul class="w3-ul w3-border w3-white w3-text-grey">
+        <li class="w3-padding">Egnland/London <span class="w3-tag w3-red w3-margin-left">Sold out</span></li>
+        <li class="w3-padding">Egnland/Yorkshire <span class="w3-tag w3-red w3-margin-left">Sold out</span></li>
+        <li class="w3-padding">USA/CA/Baverly Hills <span class="w3-badge w3-right w3-margin-right">3</span></li>
+      </ul>
+
+      <div class="w3-row-padding w3-padding-32" style="margin:0 -16px">
+        <div class="w3-third w3-margin-bottom">
+          <img src="/w3images/newyork.jpg" alt="New York" style="width:100%" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>New York</b></p>
+            <p class="w3-opacity">Fri 27 Nov 2016</p>
+            <p>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+            <button class="w3-button w3-black w3-margin-bottom" onclick="document.getElementById('ticketModal').style.display='block'">Contact</button>
+          </div>
+        </div>
+        <div class="w3-third w3-margin-bottom">
+          <img src="/w3images/paris.jpg" alt="Paris" style="width:100%" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>Paris</b></p>
+            <p class="w3-opacity">Sat 28 Nov 2016</p>
+            <p>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+            <button class="w3-button w3-black w3-margin-bottom" onclick="document.getElementById('ticketModal').style.display='block'">Contact</button>
+          </div>
+        </div>
+
+         <!-- The Contact Section -->
+  <div class="w3-container w3-content w3-padding-64"   id="empty" style="max-width:800px" id="contact">
+    <h2 class="w3-wide w3-center">CONTACT</h2>
+      <p class="w3-opacity w3-center"><i>We are here for help you</i></p>
+    <div class="w3-row w3-padding-32">
+      <div class="w3-col m6 w3-large w3-margin-bottom">
+        <i class="fa fa-map-marker" style="width:30px"></i> Newyork,NY US<br>
+        <i class="fa fa-phone" style="width:30px"></i> Phone: +00 151515<br>
+        <i class="fa fa-envelope" style="width:30px"> </i> Email: greathill@info.com<br>
+      </div>
+      <div class="w3-col m6">
+        <form action="/action_page.php" target="_blank">
+          <div class="w3-row-padding" style="margin:0 -16px 8px -16px">
+            <div class="w3-half">
+              <input class="w3-input w3-border" type="text" placeholder="Name" required name="Name">
+            </div>
+            <div class="w3-half">
+              <input class="w3-input w3-border" type="text" placeholder="Email" required name="Email">
+            </div>
+          </div>
+          <input class="w3-input w3-border" type="text" placeholder="Message" required name="Message">
+          <button class="w3-button w3-black w3-section w3-right" type="submit">SEND</button>
+        </form>
+      </div>
+    </div>
+  </div>
+        <div class="w3-third w3-margin-bottom">
+          <img src="/w3images/sanfran.jpg" alt="San Francisco" style="width:100%" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>San Francisco</b></p>
+            <p class="w3-opacity">Sun 29 Nov 2016</p>
+            <p>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+            <button class="w3-button w3-black w3-margin-bottom" onclick="document.getElementById('ticketModal').style.display='block'">Contact</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+    <br><br><br>
+    <hr><br>
+
+
+    <div class="container w-25 h-50 bg-dark"></div>
+    <div class="container w-25 h-50 bg-dark"></div>
+    <div class="container w-25 h-50 bg-dark"></div>
+    <div class="container w-25 h-50 bg-dark"></div>
+
+    <div id="mavi"></div>
+    </div>
+
+<!-- Image of location/map -->
+<img src="/w3images/map.jpg" class="w3-image w3-greyscale-min" style="width:100%">
+
+<!-- Footer -->
+<footer class="w3-container w3-padding-64 w3-center w3-opacity w3-light-grey w3-xlarge">
+  <i class="fa fa-facebook-official w3-hover-opacity"></i>
+  <i class="fa fa-instagram w3-hover-opacity"></i>
+  <i class="fa fa-snapchat w3-hover-opacity"></i>
+  <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+  <i class="fa fa-twitter w3-hover-opacity"></i>
+  <i class="fa fa-linkedin w3-hover-opacity"></i>
+  <p class="w3-medium">Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">greathil.inc</a></p>
+</footer>
+
+<script>
+// Automatic Slideshow - change image every 4 seconds
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 4000);    
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+var modal = document.getElementById('ticketModal');
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
+
+</body>
+<script>
+    var isBlue = false;
+    var blue = document.getElementById('mavi');
+
+    setInterval(function() {
+        if (isBlue) {
+            blue.style.background = 'green';
+            isblue = false;
+        } else {
+            blue.style.background = 'blue';
+            isblue = true;
+        }
+    }, 1000);
+
+</script>
+
+</html>
